a3db53a4e7b4f630896817cff5784bb5c2134efc
Jryuann <103395895+Jryuann@users.noreply.github.com>
2022-07-03 02:00:07 +0800

    buildin

--- js/gsap_btns.js
+++ js/gsap_btns.js
@@ -1,3 +1,3 @@
 document.querySelector("#btn_scroll_down").addEventListener("click", () => {
-    gsap.to(window, { duration: 1.5, scrollTo: ".skill" })
+    gsap.to(window, { duration: 1.5, scrollTo: ".projects_title" })
 })
